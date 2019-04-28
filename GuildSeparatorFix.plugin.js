e96d9cd92d6146a1292ccab14df5333da5bc2bce
Mark Fisher <markf0298@gmail.com>
2019-04-28 22:21:45 +0100

    Initial Commit

---
+++ GuildSeparatorFix.plugin.js
@@ -0,0 +1,27 @@
+//META{"name":"GuildSeparatorFix"}*//
+
+var GuildSeparatorFix = () => {};
+
+GuildSeparatorFix.prototype.start = () => {
+  $('.guildSeparator-3s64Iy').parent().addClass('guildSeparator');
+};
+
+GuildSeparatorFix.prototype.onSwitch = () => {
+  $('.guildSeparator-3s64Iy').parent().addClass('guildSeparator');
+}
+
+GuildSeparatorFix.prototype.getName = () => {
+  return "GuildSeparatorFix";
+};
+
+GuildSeparatorFix.prototype.getDescription = () => {
+  return "A fix to allow the guild separator to be styled separately.";
+};
+
+GuildSeparatorFix.prototype.getVersion = () => {
+  return "1.0.0";
+};
+
+GuildSeparatorFix.prototype.getAuthor = () => {
+  return "PoroUsedSnax";
+};
