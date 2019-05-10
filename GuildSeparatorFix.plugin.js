0779329a5ee2b508b4f7f1603fbd2171e4325329
Mark Fisher <markf0298@gmail.com>
2019-05-10 03:17:29 +0100

    Change code style and add the stop function

--- GuildSeparatorFix.plugin.js
+++ GuildSeparatorFix.plugin.js
@@ -1,27 +1,22 @@
 //META{"name":"GuildSeparatorFix"}*//
 
-var GuildSeparatorFix = function () {};
+class GuildSeparatorFix {
+  getName() {return 'GuildSeparatorFix';}
+  getDescription() {return 'A fix to allow the guild separator to be styled separately';}
+  getVersion() {return '1.1.0';}
+  getAuthor() {return 'PoroUsedSnax';}
 
-GuildSeparatorFix.prototype.start = () => {
-  $('.guildSeparator-3s64Iy').parent().addClass('guildSeparator');
-};
+  load() {}
 
-GuildSeparatorFix.prototype.onSwitch = () => {
-  $('.guildSeparator-3s64Iy').parent().addClass('guildSeparator');
+  start() {
+    $('.guildSeparator-3s64Iy').parent().addClass('guildSeparator');
+  }
+
+  stop() {
+    $('.guildSeparator-3s64Iy').parent().removeClass('guildSeparator');
+  }
+
+  onSwitch() {
+    $('.guildSeparator-3s64Iy').parent().addClass('guildSeparator');
+  }
 }
-
-GuildSeparatorFix.prototype.getName = () => {
-  return "GuildSeparatorFix";
-};
-
-GuildSeparatorFix.prototype.getDescription = () => {
-  return "A fix to allow the guild separator to be styled separately.";
-};
-
-GuildSeparatorFix.prototype.getVersion = () => {
-  return "1.0.0";
-};
-
-GuildSeparatorFix.prototype.getAuthor = () => {
-  return "PoroUsedSnax";
-};
